38fe8a1e30314be3e4d84e16988d13101871dbcc
Jason Lu <jasonlu2907@gmail.com>
2021-06-17 22:19:49 -0500

    Building the bookmarks UI

---
+++ src/app/bookmarks/bookmarks-title/bookmarks-title.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bookmarks-title',
+  templateUrl: './bookmarks-title.component.html',
+  styleUrls: ['./bookmarks-title.component.scss']
+})
+export class BookmarksTitleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
