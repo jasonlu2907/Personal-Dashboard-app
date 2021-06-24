7112a44f19844ab8d652d73aa81adaec1d078dda
Jason Lu <jasonlu2907@gmail.com>
2021-06-24 16:40:25 -0500

    Finish Dashboard UI, creating tabs, setting routes, using material icons from GG

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookmarksComponent } from './bookmarks/bookmarks.component';
+import { NotesComponent } from './notes/notes.component';
+import { TodosComponent } from './todos/todos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'bookmarks', component: BookmarksComponent},
+  {path: 'todos', component: TodosComponent},
+  {path: 'notes', component: NotesComponent},
+  { path: '', pathMatch: 'full', redirectTo: 'all'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookmarksComponent } from './bookmarks/bookmarks.component';
+import { NotesComponent } from './notes/notes.component';
+import { TodosComponent } from './todos/todos.component';
+import { TabsComponent } from './tabs/tabs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookmarksComponent,
+    NotesComponent,
+    TodosComponent,
+    TabsComponent
   ],
   imports: [
     BrowserModule,
